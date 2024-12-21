8d22438f8036e727212d45e71fbe211237d5fa1d
渡邉将太 <shotawebcoding@gmail.com>
2024-12-21 17:36:16 +0900

    feat:NewsListの作成

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import ButtonLink from "./_components/bottonLink";
+import NewsList from "./_components/newsList";
 
 type News = {
   id: string;
@@ -28,31 +29,7 @@ export default function Home() {
       </section>
       <section className={styles.news}>
         <h2 className={styles.newsTitle}>News</h2>
-        <ul>
-          {sliceData.map((article) => (
-            <li key={article.id} className={styles.list}>
-              <div className={styles.link}>
-                <Image
-                  className={styles.image}
-                  src='/no-image.png'
-                  alt='No Image'
-                  width={1200}
-                  height={630}
-                />
-                <dl className={styles.content}>
-                  <dt className={styles.newsItemTitle}>{article.title}</dt>
-                  <dd className={styles.meta}>
-                    <span className={styles.tag}>{article.category.name}</span>
-                    <span className={styles.date}>
-                      <Image src='/clock.svg' alt='' width={16} height={16} />
-                      {article.publishedAt}
-                    </span>
-                  </dd>
-                </dl>
-              </div>
-            </li>
-          ))}
-        </ul>
+        <NewsList news={sliceData} />
         <div className={styles.newsLink}>
           <ButtonLink href='/news'>もっとみる</ButtonLink>
         </div>
